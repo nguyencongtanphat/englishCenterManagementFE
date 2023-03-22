5624cc83eb82ec092e49bac93359fdd066c2d2a8
Phát Nguyễn Công Tấn <20521734@ms.uit.edu.vn>
2023-03-22 11:45:14 +0700

    complete Error Page

--- src/modules/errorPage/ErrorPage.jsx
+++ src/modules/errorPage/ErrorPage.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import Button from "react-bootstrap/Button";
+
+import errorImg from "../../assets/images/global/error.png"
 
 function ErrorPage() {
   return (
-    <div>ErrorPage</div>
-  )
+    <div className="text-center">
+      <img className="w-3/4 " src={errorImg} alt="errorimage" />
+      <h3>Page not found</h3>
+      <p>
+        Oops! Looks like you followed a bad link. If you think this is a problem
+        with us, please tell us.
+      </p>
+      <Button variant="primary">Go back home</Button>
+    </div>
+  );
 }
 
 export default ErrorPage
