dc6666d9ef043590c57beb2e781a892ab1cdf787
Phát Nguyễn Công Tấn <20521734@ms.uit.edu.vn>
2023-03-22 08:07:38 +0700

    complete setup tailwind

--- src/modules/homePage/screens/HomePage.jsx
+++ src/modules/homePage/screens/HomePage.jsx
@@ -4,7 +4,7 @@ import { Container, Row, Col } from "react-bootstrap";
 function HomePage() {
   return (
     <Container fluid="md">
-      Cente Dashboard
+      <h1 className="text-red-500">Cente Dashboard</h1>
       <Row>
         <Col>Cente Dashboard</Col>
         <Col>picker</Col>
