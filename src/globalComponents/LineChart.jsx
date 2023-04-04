b507a2baebdc6a487d605f1f8fa6797477f80a71
Phát Nguyễn Công Tấn <20521734@ms.uit.edu.vn>
2023-04-04 15:26:05 +0700

    change the width of chart to relative

--- src/globalComponents/LineChart.jsx
+++ src/globalComponents/LineChart.jsx
@@ -7,6 +7,7 @@ import {
   CartesianGrid,
   Tooltip,
   Legend,
+  ResponsiveContainer,
 } from "recharts";
 
 const data = [
@@ -20,25 +21,28 @@ const data = [
 
 const AppLineChart = () => {
   return (
-    <LineChart 
-      width={900}
-      height={300}
-      data={data}
-      margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
-    >
-      <XAxis dataKey="name" />
-      <YAxis />
-      <CartesianGrid strokeDasharray="3 3" />
-      <Tooltip />
-      <Legend />
-      <Line
-        type="monotone"
-        dataKey="score"
-        stroke="#1C64F2"
-        strokeWidth="3"
-        activeDot={{ r: 8 }}
-      />
-    </LineChart>
+    <ResponsiveContainer width="95%" height={300}>
+      <LineChart
+        className="mx-auto"
+        width="100%"
+        height="100%"
+        data={data}
+        margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
+      >
+        <XAxis dataKey="name" />
+        <YAxis />
+        <CartesianGrid strokeDasharray="3 3" />
+        <Tooltip />
+        <Legend />
+        <Line
+          type="monotone"
+          dataKey="score"
+          stroke="#1C64F2"
+          strokeWidth="3"
+          activeDot={{ r: 8 }}
+        />
+      </LineChart>
+    </ResponsiveContainer>
   );
 };
 
