1892d12e4de90c8d82af029cc4b0c4e7f5828084
Phát Nguyễn Công Tấn <20521734@ms.uit.edu.vn>
2023-04-04 15:48:21 +0700

    change shadow card

--- src/globalComponents/LineChart.jsx
+++ src/globalComponents/LineChart.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Card } from "react-bootstrap";
 import {
   LineChart,
   Line,
@@ -9,6 +10,7 @@ import {
   Legend,
   ResponsiveContainer,
 } from "recharts";
+import AppCard from "./Card";
 
 const data = [
   { name: "01 Apr", score: 80 },
@@ -21,28 +23,31 @@ const data = [
 
 const AppLineChart = () => {
   return (
-    <ResponsiveContainer width="95%" height={300}>
-      <LineChart
-        className="mx-auto"
-        width="100%"
-        height="100%"
-        data={data}
-        margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
-      >
-        <XAxis dataKey="name" />
-        <YAxis />
-        <CartesianGrid strokeDasharray="3 3" />
-        <Tooltip />
-        <Legend />
-        <Line
-          type="monotone"
-          dataKey="score"
-          stroke="#1C64F2"
-          strokeWidth="3"
-          activeDot={{ r: 8 }}
-        />
-      </LineChart>
-    </ResponsiveContainer>
+    <AppCard className="p-6">
+      <p className="font-bold text-xl">Overall</p>
+      <ResponsiveContainer width="100%" height={300}>
+        <LineChart
+          className="mx-auto"
+          width="100%"
+          height="100%"
+          data={data}
+          margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
+        >
+          <XAxis dataKey="name" />
+          <YAxis />
+          <CartesianGrid strokeDasharray="3 3" />
+          <Tooltip />
+          <Legend />
+          <Line
+            type="monotone"
+            dataKey="score"
+            stroke="#1C64F2"
+            strokeWidth="3"
+            activeDot={{ r: 8 }}
+          />
+        </LineChart>
+      </ResponsiveContainer>
+    </AppCard>
   );
 };
 
