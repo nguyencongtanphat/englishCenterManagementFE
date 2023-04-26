9af0e58a8c5b48cbf165c22ebe71e1c7cbbb88c3
levanthien1812 <20521947@gm.uit.edu.vn>
2023-04-26 17:32:16 +0700

    call get,post api periodic tests

--- src/service.js
+++ src/service.js
@@ -10,30 +10,37 @@ class StudentService {
   }
 
   getStudentsByClass(classId) {
-    return axios.get(`http://localhost:3001/api/v1/students?classId=IELTS90`)
+    return axios.get(`http://localhost:3001/api/v1/students?classId=TOE700.1`)
   }
 }
 export default new StudentService();
 
 export class StatisticsService {
+  // replace TOE700.1 with classId later
   static getAttendances(classId) {
-    return axios.get(`http://localhost:3001/api/v1/statistics/attendances/IELTS90`)
+    return axios.get(`http://localhost:3001/api/v1/statistics/attendances/TOE700.1`)
   }
-  
+
   static getHomework(classId) {
-    return axios.get(`http://localhost:3001/api/v1/statistics/homework/IELTS90`)
+    return axios.get(`http://localhost:3001/api/v1/statistics/homework/TOE700.1`)
   }
 
   static getPeriodicTests(classId) {
-    return axios.get(`http://localhost:3001/api/v1/statistics/tests/IELTS90`)
+    return axios.get(`http://localhost:3001/api/v1/statistics/tests/TOE700.1`)
+  }
+
+  static postPeriodicTest(tests) {
+    return axios.post(`http://localhost:3001/api/v1/statistics/tests/TOE700.1`, {
+      tests
+    })
   }
 }
 
 export class TestsService {
   static getHomework(classId) {
-    return axios.get(`http://localhost:3001/api/v1/tests/homework/IELTS90`)
+    return axios.get(`http://localhost:3001/api/v1/tests/homework/TOE700.1`)
   }
   static getPeriodicTests(classId) {
-    return axios.get(`http://localhost:3001/api/v1/tests/periodic-tests/IELTS90`)
+    return axios.get(`http://localhost:3001/api/v1/tests/periodic-tests/TOE700.1`)
   }
 }
